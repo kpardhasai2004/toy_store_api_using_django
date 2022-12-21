04147cdd8cd094bed299a3355fa05704bab03310
KAPAGANTI PARDHA SAI <kpardhasai2004@gmail.com>
2022-12-21 16:05:47 +0530

    Delete toystore directory

--- toystore/info/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class items(models.Model):
-    idno = models.PositiveBigIntegerField()
-    name = models.CharField(max_length=100)
-    company = models.CharField(max_length=225)
-    price = models.IntegerField()
-    quantity = models.IntegerField()
