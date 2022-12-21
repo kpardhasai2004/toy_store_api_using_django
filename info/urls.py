2cfbd846782ffae57b010c23156225fc5ab98959
KAPAGANTI PARDHA SAI <kpardhasai2004@gmail.com>
2022-12-22 00:16:13 +0530

    Add files via upload

--- info/urls.py
+++ info/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
 	path('', views.home, name='home'),
-    path('upload', views.upload, name='upload'),
-    path('dashboard', views.dashboard, name='dashboard'),
-    path('delete/<int:id>/',views.delete,name='delete'),
-    path('update/<int:id>/',views.update,name='update'),
+    path('upload/', views.upload, name='upload'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('dashboard/delete/<int:id>/',views.delete,name='delete'),
+    path('dashboard/update/<int:id>/',views.update,name='update'),
 ]
