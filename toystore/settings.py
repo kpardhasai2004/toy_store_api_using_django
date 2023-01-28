ff3f825a666ecf096bd359ebed7e8b58288472f4
KAPAGANTI PARDHA SAI <kpardhasai2004@gmail.com>
2023-01-28 18:37:37 +0530

    Update settings.py

--- toystore/settings.py
+++ toystore/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*************************************************'
+SECRET_KEY = 'django-insecure-!apz1&39(m#20gmbj)p*h(g-)4gm-o%8+o7=12*3(n8qjhswrj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
